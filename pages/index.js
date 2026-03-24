154206aa9345aaa3f1abdcb4b4eef9ac989d17f5
Leonardo Moretto <leonardocmoretto@gmail.com>
2026-03-24 23:03:19 +0000

    adicionada pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+    return <h1>Tenho fé em Deus que tudo vai se encaminhar para o melhor para nós. 
+        Vamos fazer nossa parte e aceitar o que Deus tem para nós. Eu te amo sem fim!
+        Se você gostou, dá uma risadinha :)</h1>
+}
+
+export default Home;
