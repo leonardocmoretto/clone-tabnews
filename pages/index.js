ee7c7dc0a7c5320f377bddd8278fe68df87b4cb9
Leonardo Moretto <leonardocmoretto@gmail.com>
2026-03-30 23:13:51 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 function Home() {
     return <h1>Tenho fé em Deus que tudo vai se encaminhar para o melhor para nós. 
-        Vamos fazer nossa parte e aceitar o que Deus tem para nós. Eu te amo sem fim!
-        Se você gostou, dá uma risadinha :)</h1>
+        Vamos fazer nossa parte e aceitar o que Deus tem para nós. Se você me ama, dá uma risadinha ❤️</h1>
 }
 
 export default Home;
